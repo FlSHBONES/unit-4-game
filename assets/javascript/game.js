3cff0639bd4b9f1876b76eda7c9a0b0842f5474e
TFisher <tgfisher26@gmail.com>
2018-08-26 14:11:00 -0400

    Click logic working - Values add up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // defining variables in order of page apperance
 var gamestart = false
 var target = 0;
-var counter = 0;
+var points = 0;
 var wins = 0;
 var losses = 0;
 var crystalone = {
@@ -37,7 +37,10 @@ crystalfour.value = values(1, 12);
 //verifying the crystal choices are diverse
 function valuecheck() {
     if (crystalone.value === crystaltwo.value | crystalone.value === crystalthree.value | crystaltwo.value === crystalthree.value | crystaltwo.value === crystalfour.value | crystalthree.value === crystalfour.value) {
-        gamestart = false;
+         crystalone.value = values(1, 12);
+        crystaltwo.value = values(1, 12);
+        crystalthree.value = values(1, 12);
+        crystalfour.value = values(1, 12);
     }
 }
 
@@ -55,7 +58,47 @@ $(document).ready(function startgame() {
     console.log(crystaltwo);
     console.log(crystalthree);
     console.log(crystalfour);
-    console.log (gamestart);
-    });
+    console.log(gamestart);
+});
 
 
+$("#crystal1").on("click", function () {
+
+    if (gamestart === true) {
+        points += crystalone.value;
+        console.log(crystalone);
+        $('#crystalpointssum').html("<p>Points: " + points + "</p>");
+    }
+});
+
+$("#crystal2").on("click", function () {
+
+    if (gamestart === true) {
+        points += crystaltwo.value;
+        console.log(crystaltwo);
+        $('#crystalpointssum').html("<p>Points: " + points + "</p>");
+        
+    }
+});
+
+
+
+$("#crystal3").on("click", function () {
+
+    if (gamestart === true) {
+        points += crystalthree.value;
+        console.log(crystalthree);
+        $('#crystalpointssum').html("<p>Points: " + points + "</p>");
+        
+    }
+});
+
+$("#crystal4").on("click", function () {
+
+    if (gamestart === true) {
+        points += crystalfour.value;
+        console.log(crystalfour);
+        $('#crystalpointssum').html("<p>Points: " + points + "</p>");
+        
+    }
+});
